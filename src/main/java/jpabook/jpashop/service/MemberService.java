b4eedafd57a0ded436a41c2778b5f9fcc0a19d59
hyena0608 <goatsfish@likelion.org>
2021-08-04 01:05:13 +0900

    order

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,20 +2,25 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     /**
      * 회원가입
      */
+    @Transactional
     public Long join(Member member) {
 
         validateDuplicateMember(member); //중복 회원 검증
@@ -31,4 +36,11 @@ public class MemberService {
     }
 
     //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
